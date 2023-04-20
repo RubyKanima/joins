8875e061603444b1a5271eaded47c195179c2a58
Tim Te <100081870+RubyKanima@users.noreply.github.com>
2023-04-20 17:15:40 +0300

    joins V1.0.3
    added left inner join and right inner join
    renamed different functions accordingly
    not complete

--- joins.py
+++ joins.py
@@ -1,25 +1,94 @@
 import logging
 
 class Joins():
+    '''
+    the `Joins` class contains a series of function for joining two lists which are not tupels
 
-    def __init__(self, left, right, attr) -> list:
-        self.left = left    
-        self.right = right
-        self.attr = attr
+    all basic joins compare the `items` from the left list and right list
+    - Parameters:
+        - `left`
+        - `right`
 
-    def _left_join(left, right):
+    all on joins compare the `attributes` from each `item` from left and right
+    - Parameters:
+        - `left`
+        - `right`
+        - `attr`
+        - `only_attr`:bool
+    '''
+
+    # join
+
+    def left_outer_join(left: any, right: any) -> list:
+        '''
+        left_outer_join contains all items from the left list which are not in the right list
+
+        `if left[item] != right[item]` -> `[left[item]]`
+        '''
         return [each for each in left if each not in right]
 
-    def _right_join(left, right):
-        return [each for each in right if each not in left]
+    def right_outer_join(left: any, right: any) -> list:
+        '''
+        right_outer_join contains all items from the left list which are not in the right list
 
-    def _inner_join(self, left, right):
+        `if right[item] != left[item]` -> `[right[item]]`
+        '''
+        return [each for each in right if each not in left]
+    
+    def inner_join(left: any, right: any) -> list:
+        '''
+        inner_join contains all items from the left list which are in the right list
+
+        `if left[item] == right[item]` -> [left[item] or right[item]]
+        '''
         return [each for each in left if each in right]
     
-    def _outer_join(self, left, right):
-        return self._left_join(left,right).extend(self._right_join(self, left, right))
+    def outer_join(left: any, right: any) -> list:
+        '''
+        outer_join contains all items from the left list which are not in the right list and all items from the right list which are not in the left list
+
+        `left_outer_join + right_outer_join`
+        '''
+        return Joins.left_outer_join(left,right).extend(Joins.right_outer_join(left, right))
     
-    def _inner_join_on(left, right, attr, only_attr = False) -> list:
+    # join on
+
+    def left_outer_join_on(left: any, right: any, attr, only_attr = False) -> list:
+        ''' missing code '''
+
+    def right_outer_join_on(left: any, right: any, attr, only_attr = False) -> list:
+        ''' missing code '''
+
+    def left_join_on(left: any, right: any, attr, only_attr = False) -> list:
+        ''' missing code'''    
+
+    def right_join_on(left: any, right: any, attr, only_attr = False) -> list:
+        ''' missing code'''
+
+    def left_inner_join_on(left: any, right: any, attr, only_attr = False):
+        '''
+        left_inner_join returns the left values of an inner_join_on
+
+        `if left_item[attribute] == right_item[attribute]` -> `[left_item]` or `[left_item[attribute]]`
+        '''
+
+        return [each[0] for each in Joins.inner_join_on(left, right, attr, only_attr)]
+    
+    def right_inner_join_on(left: any, right: any, attr, only_attr = False):
+        '''
+        right_inner_join_on  returns the right values of an inner_join_on
+
+        `if left_item[attribute] == right_item[attribute]` -> `[right_item]`or `[right_item[attribute]]`
+        '''
+
+        return [each[1] for each in Joins.inner_join_on(left, right, attr, only_attr)]
+
+    def inner_join_on(left: any, right: any, attr, only_attr = False) -> list:
+        '''
+        inner_join_on returns a list of all items from a left list whose item's attribute equals to the attribute of right list's item
+
+        `if left_item[attribute] == right_item[attribute]` -> `[[left_item],[right_item]]` or `[attribute]`
+        '''
         if not isinstance(left, type(right)):
             logging.error(f"TypeError: {type(left)} does not equal {type(right)}")
             raise TypeError
@@ -30,7 +99,7 @@ class Joins():
                 raise TypeError
             if only_attr:
                 if _type == list | dict:
-                    return [getattr(each, attr) for each in left for other in right if each[attr] == other[attr]]
+                    return [each[attr] for each in left for other in right if each[attr] == other[attr]]
                 return [getattr(each, attr) for each in left for other in right if getattr(each, attr) == getattr(other, attr)]
             else:
                 if _type == list | dict:
@@ -40,3 +109,12 @@ class Joins():
             logging.error(f"Attribute {attr} is not in {left} or {right}")
         except ValueError:
             logging.error(f"{attr} does not fit {_type}")
+
+    def outer_join_on(left: any, right: any , attr, only_attr = False) -> list:
+        ''' missing code'''
+
+        '''
+        outer_join_on returns a list of all outer_left_join_on and outer_right_join_on items
+
+        `outer_left_join_on + outer_right_join_on`
+        '''
